58fd14b00a400614a87114bb9f80b7ce86d9a246
logan <loloherbo.drekoh@gmail.com>
2024-01-16 16:34:46 +0100

    16.01.24

--- JavaScript.js
+++ JavaScript.js
@@ -96,158 +96,266 @@ $(document).ready(function () {
 });
 
 
-//------------------------------------------------ json --------------------------
+//------------------------------------------------ json --------------------------//
 
 
+
+
+
+
+//-----------------------CAT & PLATS ACCUEIL----------------------------//
 
 
 $(document).ready(function () {
-  var nombreCategoriesAffichees = 7; // Spécifiez le nombre de catégories que vous souhaitez afficher
-  var nombrePlatsAffiches = 3; // Spécifiez le nombre de plats que vous souhaitez afficher
-
-  // Utiliser Ajax pour charger les données à partir du fichier JSON
+  // Charger le fichier JSON via AJAX
   $.ajax({
-      url: 'data.json', // Assurez-vous de spécifier le bon chemin vers votre fichier JSON
-      dataType: 'json',
-      success: function (data) {
-          // Récupérer l'élément du conteneur des catégories
-          var categoriesContainer = $('#categories-container');
-
-          // Récupérer l'élément du conteneur des plats
-          var platsContainer = $('#plats-container');
-
-          // Parcourir les données et créer des cartes pour chaque catégorie
-          $.each(data.categorie, function (index, categorie) {
-              if (categorie.active.toLowerCase() === 'yes' && index < nombreCategoriesAffichees) {
-                  var categoryCard = $('<a href="commande.html" class="image-container col-lg-4 col-md-6 col-sm-10 mx-auto mb-4 d-block image-hover-effect"></a>');
-
-                  // Ajouter l'image de la catégorie à l'élément categoryCard avec la classe img-fluid
-                  categoryCard.html(`
-                      <img src="${categorie.image}" alt="${categorie.libelle}" class="w-100 img-thumbnail">
-                      <div class="category-info">
-                          <h2 class="image-text">${categorie.libelle}</h2>
-                      </div>
-                  `);
-
-                  // Ajouter l'élément categoryCard au conteneur des catégories
-                  categoriesContainer.append(categoryCard);
-              }
-          });
-
-          // Parcourir les données et créer des cartes pour chaque plat
-          $.each(data.plat, function (index, plat) {
-              if (plat.active.toLowerCase() === 'yes' && index < nombrePlatsAffiches) {
-                  var platCard = $('<a href="commande.html" class="image-container col-lg-4 col-md-6 col-sm-8 mx-auto mb-4 d-block image-hover-effect"></a>');
-
-                  // Ajouter l'image du plat à l'élément platCard avec la classe img-fluid
-                  platCard.html(`
-                      <img src="${plat.image}" alt="${plat.libelle}" class="img-thumbnail">
-                      <div class="category-info">
-                          <h2 class="image-text">${plat.libelle}</h2>
-                      </div>
-                  `);
-
-                  // Ajouter l'élément platCard au conteneur des plats
-                  platsContainer.append(platCard);
-              }
-          });
-      },
-      error: function (error) {
-          console.log('Erreur lors du chargement des données JSON');
-          console.log(error);
-      }
+    url: 'data.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      // Appelé une fois que le fichier JSON est chargé avec succès
+      var numberOfCategoriesToShow = 6; // Spécifiez le nombre de catégories à afficher
+      createCategoryElements(data.categorie, numberOfCategoriesToShow);
+    },
+    error: function (error) {
+      console.error('Erreur lors du chargement du fichier JSON :', error);
+    }
   });
+
+  // Fonction pour créer les éléments HTML à partir des données JSON
+  function createCategoryElements(categories, numberOfCategoriesToShow) {
+    // Limiter le nombre de catégories à afficher
+    categories = categories.slice(0, numberOfCategoriesToShow);
+
+    // Parcours du tableau et création des éléments HTML
+    categories.forEach(function (categorie) {
+      // Création d'une variable avec le code HTML
+      var categoryHtml = `
+      <div class="image-container col-12 col-sm-6 col-lg-4 mb-3">
+      <a href="#" class="col-12 d-flex justify-content-center">
+          <img src="${categorie.image}" alt="${categorie.libelle}" class="img-thumbnail image-hover-effect">
+          <div class="image-text">${categorie.libelle}</div>
+      </a>
+  </div>
+      `;
+
+      // Ajout de la variable au conteneur
+      $('#categories-container').append(categoryHtml);
+    });
+  }
 });
 
 
 
-// $(document).ready(function () {
-//   // Utiliser Ajax pour charger les données à partir du fichier JSON
-//   $.ajax({
-//     url: 'data.json', // Assurez-vous de spécifier le bon chemin vers votre fichier JSON
-//     dataType: 'json',
-//     success: function (data) {
-//       var menuContainer = $('#menu-container');
-//       var maxPlats = 4; // Spécifiez le nombre maximum de plats à afficher
+$(document).ready(function () {
+  // Charger le fichier JSON via AJAX
+  $.ajax({
+    url: 'data.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      // Appelé une fois que le fichier JSON est chargé avec succès
+      var numberOfPlatsToShow = 2; // Spécifiez le nombre de plats à afficher
+      createPlatElements(data.plat, numberOfPlatsToShow);
+    },
+    error: function (error) {
+      console.error('Erreur lors du chargement du fichier JSON :', error);
+    }
+  });
 
-//       // Parcourir les données des plats
-//       $.each(data.plat, function (index, plat) {
-//         if (plat.active.toLowerCase() === 'yes' && index < maxPlats) {
-//           var platCard = $('<div class=" row col-lg-6 " ></div>');
-//           platCard.append(`
-//           <div class="container row col-12 mx-auto ">
-//           <div class="card col-sm-8 col-10" style="max-width: 540px;">
-//             <div class="row g-0">
-//               <div class="col-md-4">
-//                 <img src="${plat.image}" class="img-fluid rounded-start col-12" style="height: 100%;" alt="cesar-salad">
-//               </div>
+  // Fonction pour créer les éléments HTML des plats à partir des données JSON
+  function createPlatElements(plats, numberOfPlatsToShow) {
+    // Limiter le nombre de plats à afficher
+    plats = plats.slice(0, numberOfPlatsToShow);
 
-//               <div class="col-md-8">
-//                 <div class="card-body" >
-//                   <h5 class="card-title">${plat.libelle}</h5>
-//                   <p class="card-text">${plat.description}</p>
-//                   <p class="card-text">${plat.prix}€</p>
-//                   <a href="commande.html" class="btn btn-primary">Commander</a>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           `);
+    // Parcours du tableau et création des éléments HTML
+    plats.forEach(function (plat) {
+      // Création d'une variable avec le code HTML
+      var platHtml = `
+      <div class="card col-sm-8 col-10" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${plat.image}" class="img-fluid rounded-start col-12" style="height: 100%;" alt="cesar-salad">
+        </div>
 
-//           // Ajouter la carte de plat au conteneur du menu
-//           menuContainer.append(platCard);
-//         }
-//       });
-//     },
-//     error: function (error) {
-//       console.log('Erreur lors du chargement des données JSON');
-//       console.log(error);
-//     }
-//   });
-// });
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${plat.libelle}</h5>
+            <p class="card-text">${plat.description}</p>
+            <p class="card-price">${plat.prix}€</p>
+            <a href="commande.html" class="btn btn-primary">Commander</a>
+          </div>
+        </div>
+      </div>
+    </div>
+          
+      `;
+
+      // Ajout de la variable au conteneur
+      $('#plats-container').append(platHtml);
+    });
+  }
+});
+
+
+
+//-----------------PAGE DES PLATS---------------------//
 
 
 $(document).ready(function () {
-  // Utiliser Ajax pour charger les données à partir du fichier JSON
-  $.ajax({
-    url: 'data.json', // Assurez-vous de spécifier le bon chemin vers votre fichier JSON
-    dataType: 'json',
-    success: function (data) {
-      var menuContainer = $('#menu-container');
-      var maxPlats = 4; // Spécifiez le nombre maximum de plats à afficher
+  var data;
+  var currentIndexPlats = 0;
+  var itemsPerPagePlats = 4;
 
-      // Parcourir les données des plats
-      $.each(data.plat, function (index, plat) {
-        if (plat.active.toLowerCase() === 'yes' && index < maxPlats) {
-          var platCard = $(`
-            <div  class="image-container col-lg-6 col-md-12 col-sm-10 mx-auto mb-4 d-block image-hover-effect ">
-              <div class="card col-sm-8 col-10" style="max-width: 540px;">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="${plat.image}" class="img-fluid rounded-start col-12 w-100 h-100" ; object-fit: cover;" alt="${plat.libelle}">
-                  </div>
+  function loadDataAndShowPlats() {
+    $.ajax({
+      url: 'data.json',
+      type: 'GET',
+      dataType: 'json',
+      success: function (jsonData) {
+        data = jsonData;
+        showPlats();
+      },
+      error: function (error) {
+        console.error('Erreur lors du chargement du fichier JSON :', error);
+      }
+    });
+  }
 
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${plat.libelle}</h5>
-                      <p class="card-text">${plat.description}</p>
-                      <p class="card-text">${plat.prix}€</p>
-                      <a href="commande.html" class="btn btn-primary">Commander</a>
-                    </div>
-                  </div>
-                </div>
+  function showPlats() {
+    $('#page-plats-container').empty();
+    var platsToShow = data.plat.slice(currentIndexPlats, currentIndexPlats + itemsPerPagePlats);
+
+    platsToShow.forEach(function (plat) {
+      var platHtml = `
+        <div class="card col-sm-8 col-10" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${plat.image}" class="img-fluid rounded-start col-12" style="height: 100%;" alt="${plat.libelle}">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${plat.libelle}</h5>
+                <p class="card-text">${plat.description}</p>
+                <p class="card-price">${plat.prix}€</p>
+                <a href="commande.html" class="btn btn-primary">Commander</a>
               </div>
             </div>
-          `);
+          </div>
+        </div>
+      `;
+      $('#page-plats-container').append(platHtml);
+    });
 
-          // Ajouter la carte de plat au conteneur du menu
-          menuContainer.append(platCard);
-        }
-      });
+    updatePaginationButtonsPlats();
+  }
+
+  function updatePaginationButtonsPlats() {
+    $('#precedent-btn-plats').prop('disabled', currentIndexPlats === 0);
+    $('#suivant-btn-plats').prop('disabled', currentIndexPlats + itemsPerPagePlats >= data.plat.length);
+  }
+
+  function navigatePlats(offset) {
+    currentIndexPlats += offset;
+    if (currentIndexPlats < 0) {
+      currentIndexPlats = 0;
+    } else if (currentIndexPlats >= data.plat.length) {
+      currentIndexPlats = data.plat.length - itemsPerPagePlats;
+    }
+    showPlats();
+  }
+
+  $('#precedent-btn-plats').on('click', function (event) {
+    event.preventDefault();
+    navigatePlats(-itemsPerPagePlats);
+  });
+
+  $('#suivant-btn-plats').on('click', function (event) {
+    event.preventDefault();
+    navigatePlats(itemsPerPagePlats);
+  });
+
+  // Charger le fichier JSON via AJAX et afficher les plats
+  loadDataAndShowPlats();
+});
+
+
+
+//------------------------PAGE categorie-------------------------//
+
+
+$(document).ready(function () {
+  var data; // Variable pour stocker les données JSON
+  var currentIndex = 0; // Indice actuel pour la pagination
+  var itemsPerPage = 6; // Nombre d'éléments à afficher par page
+
+  // Charger le fichier JSON via AJAX
+  $.ajax({
+    url: 'data.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function (jsonData) {
+      // Appelé une fois que le fichier JSON est chargé avec succès
+      data = jsonData;
+      showCategories();
     },
     error: function (error) {
-      console.log('Erreur lors du chargement des données JSON');
-      console.log(error);
+      console.error('Erreur lors du chargement du fichier JSON :', error);
+    }
+  });
+
+  // Fonction pour afficher les catégories en fonction de l'indice actuel
+  function showCategories() {
+    // Effacer le contenu actuel
+    $('#page-categories-container').empty();
+
+    // Obtenir les catégories à afficher pour la page actuelle
+    var categoriesToShow = data.categorie.slice(currentIndex, currentIndex + itemsPerPage);
+
+    // Parcourir et créer des éléments HTML pour chaque catégorie
+    categoriesToShow.forEach(function (categorie) {
+      var categoryHtml = `
+        <div class="image-container col-12 col-sm-6 col-lg-4 mb-3">
+          <a href="#" class="col-12 d-flex justify-content-center">
+            <img src="${categorie.image}" alt="${categorie.libelle}" class="img-thumbnail image-hover-effect">
+            <div class="image-text">${categorie.libelle}</div>
+          </a>
+        </div>
+      `;
+
+      // Ajouter la catégorie au conteneur
+      $('#page-categories-container').append(categoryHtml);
+    });
+
+    // Mettre à jour l'état des boutons Précédent/Suivant
+    updatePaginationButtons();
+  }
+
+  // Fonction pour mettre à jour l'état des boutons Précédent/Suivant
+  function updatePaginationButtons() {
+    $('#precedent-btn').prop('disabled', currentIndex === 0);
+    $('#suivant-btn').prop('disabled', currentIndex + itemsPerPage >= data.categorie.length);
+  }
+
+  // Gestionnaire d'événements pour le bouton Précédent
+  $('#precedent-btn').on('click', function (event) {
+    event.preventDefault();
+    if (currentIndex > 0) {
+      currentIndex -= itemsPerPage;
+      showCategories();
+    }
+  });
+
+  // Gestionnaire d'événements pour le bouton Suivant
+  $('#suivant-btn').on('click', function (event) {
+    event.preventDefault();
+    if (currentIndex + itemsPerPage < data.categorie.length) {
+      currentIndex += itemsPerPage;
+      showCategories();
     }
   });
 });
+
+
+
+
